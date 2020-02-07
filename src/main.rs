106a62dc0a45574cf6bc2b9a5f6f9d75383b9c11
Luiz de Milon <luiz.de.milon@gmail.com>
2020-02-07 07:16:03 -0400

    handle multiple connections concurrently

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
 
     // This handles all incoming connections.
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        let mut buffer = [0; 512];
-        // Having connected, now we need to read multiple times, until the connection is closed.
-        loop {
-            stream.read(&mut buffer).unwrap();
-            println!("received: {}", String::from_utf8_lossy(&buffer[..]));
+        thread::spawn(move || {
+            let mut stream = stream.unwrap();
+            let mut buffer = [0; 512];
+            // Having connected, now we need to read multiple times, until the connection is closed.
+            loop {
+                stream.read(&mut buffer).unwrap();
+                println!("received: {}", String::from_utf8_lossy(&buffer[..]));
 
-            stream.write(&buffer[..]).unwrap();
-            stream.flush().unwrap();
-        }
+                stream.write(&buffer[..]).unwrap();
+                stream.flush().unwrap();
+            }
+        });
     }
 }
